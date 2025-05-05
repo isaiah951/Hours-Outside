8eb49b800a6fccf7e26bbee444b55eefceb5f274
isaiah951 <isaiahhallmark@gmail.com>
2025-05-05 14:24:14 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
-from flask import Flask, render_template
+import os
+import sqlite3
+from flask import Flask, flash, redirect, render_template, request, session, jsonify
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 # Define a route for the homepage
 @app.route('/')
